c681aded8837caebc7af2e616ee16b42ce4a03d8
Alberto Min <director@itgatewaycloud.com>
2024-07-25 20:10:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+      <title>Learn-Zones</title>
+      <meta content="" name="description">
+      <meta content="" name="keywords">
+
+      <!-- Favicons -->
+      <link href="assets/img/logo.png" rel="icon">
+      <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+      <!-- Google Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+      <!-- Vendor CSS Files -->
+      <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+      <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+      <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+      <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+      <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+      <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+      <!-- Template Main CSS File -->
+      <link href="assets/css/style.css" rel="stylesheet">
+   </head>
+   <body>
+      <!-- ======= Header ======= -->
+      <header id="header" class="fixed-top ">
+        <div class="container d-flex align-items-center justify-content-lg-between">
+
+      <h1><a class="logo me-auto me-lg-0" href="index.html"><img src="assets/img/logo.png"> Learn-Zones</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">Category</a></li>
+          <li><a class="nav-link scrollto" href="#services">Solutions</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Pricing</a></li>
+          <li><a class="nav-link scrollto" href="#team">Resources</a></li>
+          <li class="dropdown"><a href="#"><span>About</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Login</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="#about" class="get-started-btn scrollto">Sign Up</a>
+
+        </div>
+      </header>
+      <!-- ======= End Header ======= -->
+
+      <!-- ======= Hero Section ======= -->
+      <section id="hero" class="d-flex align-items-center justify-content-center">
+        <div class="container" data-aos="fade-up">
+
+      <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+        <div class="col-xl-6 col-lg-8">
+          <h1>Powerful Digital Solutions With Gp<span>.</span></h1>
+          <h2>We are team of talented digital marketers</h2>
+        </div>
+      </div>
+
+      <div class="row gy-4 mt-5 justify-content-center" data-aos="zoom-in" data-aos-delay="250">
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-store-line"></i>
+            <h3><a href="">Lorem Ipsum</a></h3>
+          </div>
+        </div>
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-bar-chart-box-line"></i>
+            <h3><a href="">Dolor Sitema</a></h3>
+          </div>
+        </div>
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-calendar-todo-line"></i>
+            <h3><a href="">Sedare Perspiciatis</a></h3>
+          </div>
+        </div>
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-paint-brush-line"></i>
+            <h3><a href="">Magni Dolores</a></h3>
+          </div>
+        </div>
+        <div class="col-xl-2 col-md-4">
+          <div class="icon-box">
+            <i class="ri-database-2-line"></i>
+            <h3><a href="">Nemos Enimade</a></h3>
+          </div>
+        </div>
+      </div>
+
+        </div>
+      </section>
+      <!-- ======= End Hero Section ======= -->
+
+	  <!-- ======= Services Section ======= -->
+	  <section id="services" class="services-area area-padding">
+		<div class="container">
+		  <div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+			  <div class="section-headline services-head text-center">
+				<h2>Our Services</h2>
+			  </div>
+			</div>
+		  </div>
+		  <div class="row text-center">
+			<!-- Start Left services -->
+			<div class="col-md-4 col-sm-4 col-xs-12">
+			  <div class="about-move">
+				<div class="services-details">
+				  <div class="single-services">
+					<a class="services-icon" href="#">
+					  <i class="bi bi-briefcase"></i>
+					</a>
+					<h4>Expert Coder</h4>
+					<p>
+					  will have to make sure the prototype looks finished by inserting text or photo.make sure the prototype looks finished by.
+					</p>
+				  </div>
+				</div>
+				<!-- end about-details -->
+			  </div>
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-12">
+			  <div class="about-move">
+				<div class="services-details">
+				  <div class="single-services">
+					<a class="services-icon" href="#">
+					  <i class="bi bi-card-checklist"></i>
+					</a>
+					<h4>Creative Designer</h4>
+					<p>
+					  will have to make sure the prototype looks finished by inserting text or photo.make sure the prototype looks finished by.
+					</p>
+				  </div>
+				</div>
+				<!-- end about-details -->
+			  </div>
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-12">
+			  <!-- end col-md-4 -->
+			  <div class=" about-move">
+				<div class="services-details">
+				  <div class="single-services">
+					<a class="services-icon" href="#">
+					  <i class="bi bi-bar-chart"></i>
+					</a>
+					<h4>Wordpress Developer</h4>
+					<p>
+					  will have to make sure the prototype looks finished by inserting text or photo.make sure the prototype looks finished by.
+					</p>
+				  </div>
+				</div>
+				<!-- end about-details -->
+			  </div>
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-12">
+			  <!-- end col-md-4 -->
+			  <div class=" about-move">
+				<div class="services-details">
+				  <div class="single-services">
+					<a class="services-icon" href="#">
+					  <i class="bi bi-binoculars"></i>
+					</a>
+					<h4>Social Marketer </h4>
+					<p>
+					  will have to make sure the prototype looks finished by inserting text or photo.make sure the prototype looks finished by.
+					</p>
+				  </div>
+				</div>
+				<!-- end about-details -->
+			  </div>
+			</div>
+			<!-- End Left services -->
+			<div class="col-md-4 col-sm-4 col-xs-12">
+			  <!-- end col-md-4 -->
+			  <div class=" about-move">
+				<div class="services-details">
+				  <div class="single-services">
+					<a class="services-icon" href="#">
+					  <i class="bi bi-brightness-high"></i>
+					</a>
+					<h4>Seo Expart</h4>
+					<p>
+					  will have to make sure the prototype looks finished by inserting text or photo.make sure the prototype looks finished by.
+					</p>
+				  </div>
+				</div>
+				<!-- end about-details -->
+			  </div>
+			</div>
+			<!-- End Left services -->
+			<div class="col-md-4 col-sm-4 col-xs-12">
+			  <!-- end col-md-4 -->
+			  <div class=" about-move">
+				<div class="services-details">
+				  <div class="single-services">
+					<a class="services-icon" href="#">
+					  <i class="bi bi-calendar4-week"></i>
+					</a>
+					<h4>24/7 Support</h4>
+					<p>
+					  will have to make sure the prototype looks finished by inserting text or photo.make sure the prototype looks finished by.
+					</p>
+				  </div>
+				</div>
+				<!-- end about-details -->
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </section>>
+	  <!-- ======= End Services Section ======= -->
+
+	  <!--<div id="preloader"></div>-->
+      <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+      <!-- Vendor JS Files -->
+      <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+      <script src="assets/vendor/aos/aos.js"></script>
+      <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+      <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+      <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+      <script src="assets/vendor/php-email-form/validate.js"></script>
+
+      <!-- Template Main JS File -->
+      <script src="assets/js/main.js"></script>
+   </body>
+</html>
